a386ed18688e4a0630dde5aaacb3b59157505be5
Raj Kumar <raj_abc38@yahoo.com>
2024-05-20 11:50:53 -0400

    Controller level validation specs

--- spec/requests/dogs_spec.rb
+++ spec/requests/dogs_spec.rb
@@ -43,4 +43,22 @@ describe 'Dogs', type: :request do
       end
     end
   end
+
+  context 'negative cases' do
+    context 'validations' do
+      shared_examples 'name validation error' do |breed|
+        it 'words' do
+          get path, params: { breed: }
+
+          expect(response.body).to include('Please enter a valid Breed Name')
+        end
+      end
+
+      # When input breed name has more than 2 words
+      it_behaves_like 'name validation error', 'invalid breed name'
+
+      # When input breed name is blank
+      it_behaves_like 'name validation error', ''
+    end
+  end
 end
